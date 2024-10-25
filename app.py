3687bafc088c725986d245d1b26a8f6f43aed08e
MihaiNicolae1 <mihaidavid118@gmail.com>
2024-10-25 11:23:30 +0300

    Changes to server to conver heif or heic

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 import time
+import pyheif
+import io
+
 from flask import Flask, request, send_file
 from rembg import remove, new_session
 from flask_cors import CORS
@@ -24,6 +27,16 @@ def upload_file():
         return "No selected file"
 
     input = file.read()
+    # if file is type heif or heic, convert to png
+    name_lower = file.filename.lower()
+    # check if file type is supported
+    supported_types = ['.png', '.jpg', '.jpeg', '.heif', '.heic']
+    if not any(name_lower.endswith(t) for t in supported_types):
+        return "Unsupported file type"
+
+    if name_lower.endswith('.heif') or name_lower.endswith('.heic'):
+        input = convert_heif(input)
+
     output = remove(input, session=session)
 
     output_path = 'output.png'
@@ -32,6 +45,12 @@ def upload_file():
 
     return send_file(output_path, as_attachment=True)
 
+def convert_heif(input):
+    heif_file = pyheif.read_heif(io.BytesIO(input))
+    image = heif_file.convert_to_pil()
+    image.save('temp.png', 'PNG')
+    with open('temp.png', 'rb') as f:
+        return f.read()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
